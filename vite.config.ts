3c6c3d4e47e72bad75396bcbee042946e4cf956b
Arthur Kok <Arthur.Kok@dayforce.com>
2025-10-04 01:02:31 -0400

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/moon-plane-transit/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
